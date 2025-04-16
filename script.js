11dd23e8dd96a262434d0398c6abad22caea03d2
SANTHOSH MATHAVAN R <santhoshr292003@gmail.com>
2025-04-16 16:33:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function functio(small){
+    var full=document.getElementById("imagebox")
+    full.src = small.src
+    // small.src=full.src
+}
+function facebook(){
+    window.location.href="https://www.facebook.com/login/" 
+}
+function instagram(){
+    window.location.href="https://www.instagram.com/accounts/login/?hl=en"
+}
+function twitter(){
+    window.location.href="https://x.com/i/flow/login"
+}
+function linkedin(){
+    window.location.href="https://www.linkedin.com/login"
+}
+function nike(){
+    window.location.href="https://www.nike.com/in/"
+}
+function google(){
+    window.location.href="https://www.google.co.in/"
+}
